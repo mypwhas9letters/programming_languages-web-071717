e00f57288db134090463ffd8ae6102a2219d3a0d
Hui Wang <huiw3939@gmail.com>
2017-07-15 18:06:17 -0400

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,23 @@
+ require 'pry'
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.map do |oofunc, lang|
+    lang.map do |lang, type|
+    new_hash[lang] =type
+    end
+  end
+    new_hash.map do |x,y|
+      languages.map do |oofunc, lang|
+       lang.keys.each do |language|
+         if x == language
+           if new_hash[language][:style] == nil
+           new_hash[language][:style] = [oofunc]
+         else
+          new_hash[language][:style].push(oofunc)
+        end
+      end
+    end
+      end
+  end
+  new_hash
 end
